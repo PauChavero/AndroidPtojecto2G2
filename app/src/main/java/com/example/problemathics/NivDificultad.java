38eefc88c542f8b70d39bee66a93dfa901735f1b
PauChavero <a22pauchagor@inspedralbes.cat>
2023-11-30 10:20:48 +0100

    Estado App 29/11

---
+++ app/src/main/java/com/example/problemathics/NivDificultad.java
@@ -0,0 +1,80 @@
+package com.example.problemathics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class NivDificultad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niv_dificultad);
+
+
+        Button btnFacil = findViewById(R.id.btnFacil);
+
+        Button btnInter = findViewById(R.id.btnIntermedi);
+
+        Button btnDific = findViewById(R.id.btnDificil);
+
+        btnFacil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NivDificultad.this, PregFacil.class);
+                startActivity(intent);
+            }
+        });
+
+        btnInter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NivDificultad.this, PregInter.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDific.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NivDificultad.this, PregDific.class);
+                startActivity(intent);
+            }
+        });
+    }
+    //Overflow options menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //Administrador de el directorio
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id= item.getItemId();
+
+        if(id == R.id.OpMenu1) {
+            Intent intent = new Intent(NivDificultad.this, NivDificultad.class);
+            startActivity(intent);
+
+
+        }else if(id == R.id.OpMenu2){
+            Intent intent = new Intent(NivDificultad.this,NivDificultad.class);
+            startActivity(intent);
+
+        }
+        else if (id == R.id.OpMenuSalir){
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/problemathics/PregInter.java
@@ -0,0 +1,46 @@
+package com.example.problemathics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PregInter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preg_inter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //Administrador de el directorio
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id= item.getItemId();
+
+        if(id == R.id.OpMenu1) {
+            Intent intent = new Intent(PregInter.this, NivDificultad.class);
+            startActivity(intent);
+
+
+        }else if(id == R.id.OpMenu2){
+            Intent intent = new Intent(PregInter.this,NivDificultad.class);
+            startActivity(intent);
+
+        }
+        else if (id == R.id.OpMenuSalir){
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
