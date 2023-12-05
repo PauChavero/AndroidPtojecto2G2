2a8bb3bcc9d1761a561a1ea7f3b5efde7ced6a3a
PauChavero <a22pauchagor@inspedralbes.cat>
2023-12-05 09:18:32 +0100

    +Pestañas

--- app/src/main/java/com/example/problemathics/MainActivity.java
+++ app/src/main/java/com/example/problemathics/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NivDificultad.class);
+                Intent intent = new Intent(MainActivity.this, EscUF.class);
                 startActivity(intent);
             }
         });
