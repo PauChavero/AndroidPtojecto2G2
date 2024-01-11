7a9b968228cd6940f1799ddffcd98b47874cb559
MikiDix <121021846+MikiDix@users.noreply.github.com>
2024-01-11 10:42:24 +0100

    Update PregFacil.java

--- app/src/main/java/com/example/problemathics/PregFacil.java
+++ app/src/main/java/com/example/problemathics/PregFacil.java
@@ -36,7 +36,7 @@ public class PregFacil extends AppCompatActivity {
 
     String ufSeleccionadaInt = "";
     Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:3001")
+            .baseUrl("http://problemathics.dam.inspedralbes.cat:3001")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
