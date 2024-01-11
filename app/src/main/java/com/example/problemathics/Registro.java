5e37f1d79e7171207270b5128e0a3a5ef83a99f6
MikiDix <121021846+MikiDix@users.noreply.github.com>
2024-01-11 10:43:30 +0100

    Update Registro.java

--- app/src/main/java/com/example/problemathics/Registro.java
+++ app/src/main/java/com/example/problemathics/Registro.java
@@ -46,7 +46,7 @@ public class Registro extends AppCompatActivity {
                     RegistroEnviar registroEnviar = new RegistroEnviar(nomUsuari,email,contrasenya, 0 ,500,0,0, false );
 
                     Retrofit retrofit = new Retrofit.Builder()
-                            .baseUrl("http://10.0.2.2:3001")
+                            .baseUrl("http://problemathics.dam.inspedralbes.cat:3001")
                             .addConverterFactory(GsonConverterFactory.create())
                             .build();
 
