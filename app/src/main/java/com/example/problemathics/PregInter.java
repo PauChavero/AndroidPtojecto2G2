fca3133ca50ac9abca4773b93095c8f23b3fc2a0
MikiDix <121021846+MikiDix@users.noreply.github.com>
2024-01-11 10:42:59 +0100

    Update PregInter.java

--- app/src/main/java/com/example/problemathics/PregInter.java
+++ app/src/main/java/com/example/problemathics/PregInter.java
@@ -40,7 +40,7 @@ public class PregInter extends AppCompatActivity {
 
     String ufSeleccionadaInt = "";
     Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:3001")
+            .baseUrl("http://problemathics.dam.inspedralbes.cat:3001")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
